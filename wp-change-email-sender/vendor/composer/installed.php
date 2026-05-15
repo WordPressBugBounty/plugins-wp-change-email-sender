c2179c44064f595cb5725748bc5ef14fd7545b63
Sean Murphy <sean@iamseanmurphy.com>
2026-05-15 11:00:50 +0000

    3.3.1

--- wp-change-email-sender/vendor/composer/installed.php
+++ wp-change-email-sender/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'welabs/wp-change-email-sender',
-        'pretty_version' => 'v3.3.0',
-        'version' => '3.3.0.0',
-        'reference' => '74f8d3f5da74275c8dcec7916c3bf0f0b3c4259c',
+        'pretty_version' => 'v3.3.1',
+        'version' => '3.3.1.0',
+        'reference' => 'f44b09182c9ae2716bc0487592b3df6b3b79689a',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'welabs/wp-change-email-sender' => array(
-            'pretty_version' => 'v3.3.0',
-            'version' => '3.3.0.0',
-            'reference' => '74f8d3f5da74275c8dcec7916c3bf0f0b3c4259c',
+            'pretty_version' => 'v3.3.1',
+            'version' => '3.3.1.0',
+            'reference' => 'f44b09182c9ae2716bc0487592b3df6b3b79689a',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
